3f3c3270d214bb7b631f930339f411c78145b267
Kenji <85250024+rkyakenji@users.noreply.github.com>
2023-02-22 14:48:42 -0300

    Programa para cálculo de Fatorial de um nº

---
+++ fatorial.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void fatorial(int n1);
+
+
+int main(){
+    int a;
+    cout << "\n Digite um número para ser calculado o fatorial \n";
+    cin  >> a;
+
+
+
+    fatorial(a);
+
+
+    return 0;
+}
+    
+void fatorial(int n1){
+    int res=1;
+    for (int i=n1;i>0;i--){
+        res = res*i;
+    }
+    cout << "\nO valor fatorial do número calculado é: " << res << endl;
+}
